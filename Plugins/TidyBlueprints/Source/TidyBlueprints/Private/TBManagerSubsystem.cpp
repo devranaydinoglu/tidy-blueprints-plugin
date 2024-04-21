c23fbe8e9814dd12f13044e1e3f97a04b292fd81
devranaydinoglu <d.s.aydinoglu@gmail.com>
2024-04-21 17:37:31 +0200

    Added classes to store data and added logic to extract node data from selected nodes

--- Plugins/TidyBlueprints/Source/TidyBlueprints/Private/TBManagerSubsystem.cpp
+++ Plugins/TidyBlueprints/Source/TidyBlueprints/Private/TBManagerSubsystem.cpp
@@ -9,7 +9,7 @@
 FBlueprintEditor* UTBManagerSubsystem::GetCurrentBlueprintEditor()
 {
 	TArray<UObject*> EditedAssets = GEditor->GetEditorSubsystem<UAssetEditorSubsystem>()->GetAllEditedAssets();
-
+	
 	for (UObject* Asset : EditedAssets)
 	{
 		IAssetEditorInstance* AssetEditorInstance = GEditor->GetEditorSubsystem<UAssetEditorSubsystem>()->FindEditorForAsset(Asset, false);
@@ -28,33 +28,146 @@ FBlueprintEditor* UTBManagerSubsystem::GetCurrentBlueprintEditor()
 
 UEdGraph* UTBManagerSubsystem::GetCurrentGraph()
 {
-	UE_LOG(LogTemp, Display, TEXT("Current Graph: %s"), *GetCurrentBlueprintEditor()->GetFocusedGraph()->GetName());
 	return GetCurrentBlueprintEditor()->GetFocusedGraph();
 }
 
-void UTBManagerSubsystem::GetSelectedNodes(UEdGraph* Graph)
+FGraphPanelSelectionSet UTBManagerSubsystem::GetSelectedNodes()
 {
-
+	CurrentBlueprintEditor = GetCurrentBlueprintEditor();
+	return CurrentBlueprintEditor->GetSelectedNodes();
 }
 
 void UTBManagerSubsystem::StartTidyUp()
 {
-	FBlueprintEditor* BlueprintEditor = GetCurrentBlueprintEditor();
-	FGraphPanelSelectionSet SelectedNodes = BlueprintEditor->GetSelectedNodes();
+	FGraphPanelSelectionSet SelectedNodes = GetSelectedNodes();
+	TBCluster Cluster;
+	int32 CollectionIndex = 0;
 
-	UE_LOG(LogTemp, Display, TEXT("BlueprintEditor Name: %s"), *BlueprintEditor->GetBlueprintObj()->GetFriendlyName());
-	GetCurrentGraph();
-	for (auto Node : SelectedNodes)
+	// TODO: fix collection index out of order (after getting first node in sequence, we should follow its ouput exec pin
+	for (UObject* NodeObj : SelectedNodes)
 	{
-		UE_LOG(LogTemp, Display, TEXT("Node Title: %s"), *Cast<UEdGraphNode>(Node)->GetNodeTitle(ENodeTitleType::FullTitle).ToString());
-		for (const UEdGraphPin* Pin : Cast<UEdGraphNode>(Node)->Pins)
+		UEdGraphNode* Node = Cast<UEdGraphNode>(NodeObj);
+
+		TBNode NodeData = PopulateNodeData(Node);
+
+		if (IsNodeExecutable(Node))
 		{
-			UE_LOG(LogTemp, Display, TEXT("Pin Name: %s"), *Pin->PinName.ToString());
-			UE_LOG(LogTemp, Display, TEXT("Pin Category: %s"), *Pin->PinType.PinCategory.ToString());
-			const UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("EEdGraphPinDirection"), true);
-			UE_LOG(LogTemp, Display, TEXT("Pin Direction: %s"), *EnumPtr->GetDisplayNameTextByValue(Pin->Direction).ToString());
+			TBCollection Collection;
+			Collection.Index = CollectionIndex;
+			Collection.ParentNode = NodeData;
+
+			// Get all non-executable nodes linked to the parent node
+			for (UEdGraphPin* Pin : Node->Pins)
+			{
+				if (Pin->PinType.PinCategory != "exec")
+				{
+					for (UEdGraphPin* LinkedPin : Pin->LinkedTo)
+					{
+						if (Pin->Direction == EGPD_Input) Collection.InputNodes.Add(PopulateNodeData(LinkedPin->GetOwningNode()));
+						else if (Pin->Direction == EGPD_Output) Collection.OutputNodes.Add(PopulateNodeData(LinkedPin->GetOwningNode()));
+						RecursivelyGetChildNodes(LinkedPin->GetOwningNode(), LinkedPin, Collection);
+					}
+				}
+			}
+
+			Cluster.Collections.Add(Collection);
+			CollectionIndex++;
+		}
+
+		if (IsNodeFirstInSequence(Node, SelectedNodes))
+		{
+			Cluster.StartingNode = PopulateNodeData(Node);
 		}
 	}
+
+	UE_LOG(LogTemp, Error, TEXT("Cluster starting node: %s"), *Cluster.StartingNode.Node.Get()->GetNodeTitle(ENodeTitleType::FullTitle).ToString());
+
+	for (int32 i = 0; i < Cluster.Collections.Num(); i++)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Collection parent node: %s"), *Cluster.Collections[i].ParentNode.Node.Get()->GetNodeTitle(ENodeTitleType::FullTitle).ToString());
+		UE_LOG(LogTemp, Display, TEXT("Collection index: %s"), *FString::FromInt(Cluster.Collections[i].Index));
+
+		for (int32 j = 0; j < Cluster.Collections[i].InputNodes.Num(); j++)
+		{
+			UE_LOG(LogTemp, Display, TEXT("Collection input node: %s"), *Cluster.Collections[i].InputNodes[j].Node.Get()->GetNodeTitle(ENodeTitleType::FullTitle).ToString());
+		}
+	}
+
+}
+
+void UTBManagerSubsystem::RecursivelyGetChildNodes(UEdGraphNode* Node, UEdGraphPin* InLinkedPin, TBCollection& Collection)
+{
+	if (Node->Pins.Num() < 2) return;
+
+	for (UEdGraphPin* Pin : Node->Pins)
+	{
+		if (Pin->PinType.PinCategory != "exec" && Pin != InLinkedPin)
+		{
+			for (UEdGraphPin* LinkedPin : Pin->LinkedTo)
+			{
+				if (Pin->Direction == EGPD_Input) Collection.InputNodes.Add(PopulateNodeData(LinkedPin->GetOwningNode()));
+				else if (Pin->Direction == EGPD_Output) Collection.OutputNodes.Add(PopulateNodeData(LinkedPin->GetOwningNode()));
+
+				RecursivelyGetChildNodes(LinkedPin->GetOwningNode(), LinkedPin, Collection);
+			}
+		}
+	}
+}
+
+TBNode UTBManagerSubsystem::PopulateNodeData(UEdGraphNode* Node)
+{
+	TBNode NodeData;
+	NodeData.Node = TStrongObjectPtr(Node);
+	FVector2D NodeSize = FVector2D(Node->NodeWidth, Node->NodeHeight);
+
+	TSharedPtr<SGraphEditor> GraphEditor = GetCurrentBlueprintEditor()->OpenGraphAndBringToFront(GetCurrentGraph());
+	FSlateRect Rect;
+	if (GraphEditor->GetBoundsForNode(Node, Rect, 0.f))
+	{
+		NodeSize = FVector2D(Rect.Right - Rect.Left, Rect.Bottom - Rect.Top);
+	}
+
+	NodeData.NodeHeight = NodeSize.Y;
+	NodeData.NodeWidth = NodeSize.X;
+
+	return NodeData;
+}
+
+bool UTBManagerSubsystem::IsNodeFirstInSequence(const UEdGraphNode* Node, FGraphPanelSelectionSet SelectedNodes)
+{
+	int32 NumExecutePins = 0;
+	for (const UEdGraphPin* ExecPin : Node->Pins)
+	{
+		if (ExecPin->PinType.PinCategory == "exec") NumExecutePins++;
+	}
+	if (Node->Pins.Num() < 3 && NumExecutePins > 0 && NumExecutePins < Node->Pins.Num()) return true;
+
+	for (const UEdGraphPin* Pin : Node->Pins)
+	{
+		if (Pin->Direction == EGPD_Input && Pin->PinName == "execute")
+		{
+			for (const UEdGraphPin* LinkedPin : Pin->LinkedTo)
+			{
+				if (!SelectedNodes.Contains(LinkedPin->GetOwningNode()))
+				{
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+bool UTBManagerSubsystem::IsNodeExecutable(const UEdGraphNode* Node)
+{
+	int32 NumExecPins = 0;
+	for (const UEdGraphPin* Pin : Node->Pins)
+	{
+		if (Pin->PinType.PinCategory == "exec") NumExecPins++;
+	}
+
+	return NumExecPins > 0;
 }
 
 void UTBManagerSubsystem::SetNodePosition(UEdGraph* Graph, UEdGraphNode* Node, const FVector2D& NewPosition)
--- Plugins/TidyBlueprints/Source/TidyBlueprints/Public/TBManagerSubsystem.h
+++ Plugins/TidyBlueprints/Source/TidyBlueprints/Public/TBManagerSubsystem.h
@@ -9,10 +9,68 @@
 class UEdGraphNode;
 class FBlueprintEditor;
 
+class UTBPin
+{
+	// Name of the pin as seen on the node
+	FName PinName;
+
+	// Data type of the pin
+	FName PinType;
+
+	// Direction of the pin on the node (input or output)
+	TEnumAsByte<enum EEdGraphPinDirection> PinDirection;
+
+	// Node which owns this pin
+	TStrongObjectPtr<UEdGraphNode> OwningNode;
+};
+
+class TBNode
+{
+public:
+	TStrongObjectPtr<UEdGraphNode> Node;
+	int32 NodeHeight;
+	int32 NodeWidth;
+};
+
+/**
+ * A node connected by execution pins and all of its non-execution input and output nodes
+ */
+class TBCollection
+{
+public:
+	// The index of this collection in the cluster
+	int32 Index;
+
+	// Node which is connected by execution pins
+	TBNode ParentNode;
+
+	// Input nodes ordered by X and Y positions on the graph
+	TArray<TBNode> InputNodes;
+
+	// Output nodes ordered by X and Y positions on the graph
+	TArray<TBNode> OutputNodes;
+};
+
+/**
+ * A sequence of nodes which are connected through pins.
+ * It should not include nodes which are not part of the execution flow of the nodes sequence.
+ */
+class TBCluster
+{
+public:
+	// First node in the sequence
+	TBNode StartingNode;
+
+	TArray<TBCollection> Collections;
+};
+
 UCLASS()
 class TIDYBLUEPRINTS_API UTBManagerSubsystem : public UEditorSubsystem
 {
 	GENERATED_BODY()
+
+private:
+	FBlueprintEditor* CurrentBlueprintEditor = nullptr;
 
 public:
 
@@ -29,7 +87,7 @@ public:
 	/**
 	 * Gets all the selected nodes in the current graph.
 	 */
-	void GetSelectedNodes(UEdGraph* Graph);
+	FGraphPanelSelectionSet GetSelectedNodes();
 
 	/**
 	 * Entry point, called when Tidy Up button is clicked.
@@ -37,6 +95,22 @@ public:
 	void StartTidyUp();
 
 private:
+
+	void RecursivelyGetChildNodes(UEdGraphNode* Node, UEdGraphPin* InLinkedPin, TBCollection& Collection);
+
+	TBNode PopulateNodeData(UEdGraphNode* Node);
+
+	/**
+	 * Checks whether the node is the first node in the selected nodes' execution sequence.
+	 * @return Is first in sequence
+	 */
+	bool IsNodeFirstInSequence(const UEdGraphNode* Node, FGraphPanelSelectionSet SelectedNodes);
+
+	/**
+	 * Checks whether the node is executable by checking its pins.
+	 * @return Executable
+	 */
+	bool IsNodeExecutable(const UEdGraphNode* Node);
 
 	/**
 	 * Updates the position of the provided node on the blueprint graph.
